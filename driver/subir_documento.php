3f97736da4d49f81566bb1c84e6c06a022c7bf15
Ing. Aldair Morales Cuellar <al-dair12@hotmail.es>
2019-08-13 17:06:12 -0500

    adtos

--- driver/subir_documento.php
+++ driver/subir_documento.php
@@ -13,6 +13,18 @@ if ( isset( $_POST[ 'documento' ] ) ) {
 		$apellido1 = utf8_decode($data->val( $i, 3 ));
 		$apellido2 = utf8_decode($data->val( $i, 4 ));
 		$identificacion = $data->val( $i, 5 );
+		$nacimiento = utf8_decode("1980-10-10");
+		$correo = utf8_decode("correo123pru".$i."@gmail.com");
+		$telefono1 = "9566".$i;
+		$telefono2 = "5986".$i;
+		$telefono3 = "4512".$i;
+		$direccion = "Ninguna";
+		$corregimiento = "Ninguno";
+		$vereda = "Ninguna";
+		$departamento = 18;
+		$municipio = 1044;
+		$sexo = 1;
+		$tipo = 2;
 
 		/*
 		$correo = utf8_decode($data->val( $i, 6 ));
@@ -33,12 +45,18 @@ if ( isset( $_POST[ 'documento' ] ) ) {
 		@$fechaactual = date( 'Y-m-d H:i:s' );
         */
 
-		if($nombre != "" || $nombre != null){
-			$insertar = "INSERT INTO categoria (nombre1, nombre2, apellido1, apellido2, identificacion, departamento_votacion, municipio_votacion, fk_zona, fk_puesto, mesa, estado, fk_campaña) VALUES ('$nombre1', '$nombre2', '$apellido1', '$apellido2', '$identificacion', '100', '1101', '6', '103', '1', '1', '8')";
+		if($nombre1 != "" || $nombre1 != null){
+			/*
+			$insertar = "DELETE FROM persona WHERE nombre1 = '$nombre1' AND nombre2 = '$nombre2' AND apellido1 = '$apellido1' AND fk_campaña = 8";
+
+			*/
+
+			
+			$insertar = "INSERT INTO persona (nombre1, nombre2, apellido1, apellido2, identificacion, nacimiento, correo, telefono1, telefono2, telefono3, direccion, fk_sexo, fk_departamento, fk_municipio, fk_referido, fk_tipo_persona, departamento_votacion, municipio_votacion, corregimiento, vereda_barrio, fk_zona, fk_puesto, mesa, estado, fk_campaña) VALUES ('$nombre1', '$nombre2', '$apellido1', '$apellido2', '$identificacion', '$nacimiento', '$correo', '$telefono1', '$telefono2', '$telefono3', '$direccion', '$sexo', '$departamento', '$municipio', '13720', '$tipo', '100', '1101', '$corregimiento', '$vereda', '6', '103', '1', '1', '8')";
 
 			if ( $conexion->query( $insertar ) == true ) {
 				unlink( $_FILES[ 'documento' ][ 'tmp_name' ] );
-				echo "se inserto: ".$nombre;
+				echo "se inserto: ".$nombre1;
 				echo "<br>";
 			/*
 			echo '<script language="javascript">
@@ -53,7 +71,7 @@ if ( isset( $_POST[ 'documento' ] ) ) {
 				</script>';
 			}
 	}else{
-		echo "campo: ".$nombre." vacio";
+		echo "campo: ".$nombre1." vacio";
 	}
 
 }
